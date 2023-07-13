ee515adb2e1f533bcc518328f77d81575ee2df4d
georgemisterio <gabipenasoy@gmail.com>
2023-07-13 17:56:37 -0300

    Main file now is edited

--- m3u8finder.py
+++ m3u8finder.py
@@ -1,30 +1,5 @@
 # import requests
 # import re
-
-# def obtener_direccion_m3u8(url):
-#     # Realiza una solicitud HTTP GET a la URL proporcionada
-#     response = requests.get(url)
-#     # Busca la dirección m3u8 en el contenido de la página usando expresiones regulares
-#     pattern = r'(https?://[^\s]+\.m3u8)'
-#     match = re.search(pattern, response.text)
-    
-#     if match:
-#         return match.group(0)
-#     else:
-#         return None
-
-# url_pagina_web = input('Ingrese la URL del video ')
-
-# # Obtén la dirección m3u8 del video
-# direccion_m3u8 = obtener_direccion_m3u8(url_pagina_web)
-
-# if direccion_m3u8:
-#     print('Dirección m3u8 del video:', direccion_m3u8)
-# else:
-#     print('No se encontró la dirección m3u8 del video.')
-
-# import requests
-# import re
 # import threading
 # import tkinter as tk
 # from tkinter import messagebox
@@ -46,7 +21,7 @@
 #         messagebox.showerror('Error', 'Por favor ingrese una URL válida.')
 #         return
 
-#     progress_bar.start()
+#     progress_bar.start(10)  # Velocidad de movimiento del ProgressBar
 
 #     # Crear un hilo aparte para realizar la búsqueda
 #     threading.Thread(target=lambda: buscar_en_hilo(url)).start()
@@ -70,32 +45,51 @@
 #     pyperclip.copy(direccion_m3u8)
 #     messagebox.showinfo('Copiado', 'Dirección m3u8 copiada al portapapeles.')
 
+# def pegar():
+#     url = window.clipboard_get()
+#     entry_url.delete(0, tk.END)
+#     entry_url.insert(0, url)
+
 # # Crear la ventana principal
 # window = tk.Tk()
 # window.title('Obtener dirección m3u8')
-# window.geometry('400x200')
+# window.geometry('500x250')
+# window.configure(bg='black')
 
 # # Crear etiqueta y entrada de texto para la URL
 # label_url = tk.Label(window, text='URL de la página web:')
 # label_url.pack()
+# label_url.configure(bg='black', fg='white')
 # entry_url = tk.Entry(window, width=50)
+# entry_url.configure(bg='grey', fg='white')
 # entry_url.pack()
 
 # # Crear botón para buscar la dirección m3u8
-# btn_buscar = tk.Button(window, text='Buscar dirección m3u8', command=buscar_direccion)
+# s = ttk.Style()
+# s.configure("Search.TButton", foreground="grey", background="#42413f")
+# s.map("Search.TButton", foreground=[("active", "cyan")], background=[("pressed", "#525151")])
+# btn_buscar = ttk.Button(window, text='Buscar dirección m3u8', style="Search.TButton", command=buscar_direccion)
 # btn_buscar.pack()
 
 # # Crear ProgressBar indefinido
-# progress_bar = ttk.Progressbar(window, mode='indeterminate')
+# progress_bar = ttk.Progressbar(window, mode='indeterminate', length=300)
 # progress_bar.pack()
 
 # # Crear etiqueta para mostrar la dirección m3u8
-# label_direccion = tk.Label(window, text='')
+# label_direccion = tk.Label(window, text='', justify='left', wraplength=380)
+# label_direccion.configure(bg='grey', fg='white',width =50)
 # label_direccion.pack()
 
 # # Crear botón para copiar al portapapeles
-# btn_copiar = tk.Button(window, text='Copiar al portapapeles', command=copiar_portapapeles)
+# btn_copiar = ttk.Button(window, text='Copiar al portapapeles', command=copiar_portapapeles, style="Search.TButton")
 # btn_copiar.pack()
+
+# # Crear menú contextual para pegar
+# menu = tk.Menu(window, tearoff=0)
+# menu.add_command(label="Pegar", command=pegar)
+
+# # Asociar el menú contextual al Entry
+# entry_url.bind("<Button-3>", lambda e: menu.tk_popup(e.x_root, e.y_root))
 
 # # Iniciar el bucle principal de la interfaz
 # window.mainloop()
@@ -106,7 +100,7 @@ import re
 import threading
 import tkinter as tk
 from tkinter import messagebox
-from tkinter import ttk
+from tkinter import ttk, PhotoImage
 import pyperclip
 
 def obtener_direccion_m3u8(url):
@@ -148,42 +142,52 @@ def copiar_portapapeles():
     pyperclip.copy(direccion_m3u8)
     messagebox.showinfo('Copiado', 'Dirección m3u8 copiada al portapapeles.')
 
+def pegar():
+    url = window.clipboard_get()
+    entry_url.delete(0, tk.END)
+    entry_url.insert(0, url)
+
 # Crear la ventana principal
 window = tk.Tk()
-window.title('Obtener dirección m3u8')
-window.geometry('400x200')
+window.title('PyM3u8Finder')
+window.geometry('500x250')
 window.configure(bg='black')
+photo = PhotoImage(file = "icon.png")
+window.iconphoto(False, photo)
 # Crear etiqueta y entrada de texto para la URL
 label_url = tk.Label(window, text='URL de la página web:')
-label_url.pack()
-label_url.configure(bg='black',fg='white')
+label_url.pack(pady=10)
+label_url.configure(bg='black', fg='white')
 entry_url = tk.Entry(window, width=50)
-entry_url.configure(bg='grey',fg='white')
-entry_url.pack()
+entry_url.configure(bg='grey', fg='white')
+entry_url.pack(pady=5)
 
 # Crear botón para buscar la dirección m3u8
-
 s = ttk.Style()
-s.configure("Search.TButton", foreground="grey",background="#42413f")
-s.map("Search.TButton", foreground=[("active", "cyan")],background=[("pressed", "#525151")])
-# boton = ttk.Button(text="¡Hola, mundo!", style="Search.TButton")
-
-btn_buscar = ttk.Button(window, text='Buscar dirección m3u8', style="Search.TButton",command=buscar_direccion)
-# btn_buscar.configure(bg='grey',fg='white')
-btn_buscar.pack()
+s.configure("Search.TButton", foreground="grey", background="#42413f")
+s.map("Search.TButton", foreground=[("active", "cyan")], background=[("pressed", "#525151")])
+btn_buscar = ttk.Button(window, text='Buscar dirección m3u8', style="Search.TButton", command=buscar_direccion)
+btn_buscar.pack(pady=5)
 
 # Crear ProgressBar indefinido
 progress_bar = ttk.Progressbar(window, mode='indeterminate', length=300)
-progress_bar.pack()
+progress_bar.pack(pady=10)
 
 # Crear etiqueta para mostrar la dirección m3u8
 label_direccion = tk.Label(window, text='', justify='left', wraplength=380)
-label_direccion.configure(bg ='grey',fg ='white')
-label_direccion.pack()
+label_direccion.configure(bg='grey', fg='white', width=50)
+label_direccion.pack(pady=5)
 
 # Crear botón para copiar al portapapeles
 btn_copiar = ttk.Button(window, text='Copiar al portapapeles', command=copiar_portapapeles, style="Search.TButton")
-btn_copiar.pack()
+btn_copiar.pack(pady=5)
+
+# Crear menú contextual para pegar
+menu = tk.Menu(window, tearoff=0)
+menu.add_command(label="Pegar", command=pegar)
+
+# Asociar el menú contextual al Entry
+entry_url.bind("<Button-3>", lambda e: menu.tk_popup(e.x_root, e.y_root))
 
 # Iniciar el bucle principal de la interfaz
 window.mainloop()
